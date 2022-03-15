ee46b2d8d0bc5d09bdb023b4b480180fc6c8b9b3
zafer <zafer@BELERIAND>
2022-03-15 21:27:11 +0300

    Add project files.

---
+++ Portfolio-WebUI/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+let menu = $(".right-menu");
+
+$(".menu-btn").click(function (e) {
+    e.preventDefault();
+
+    if (menu.css("right") != 0)
+        menu.css("right", "0");
+
+});
+$(".close-btn").click(function (e) {
+    e.preventDefault();
+    menu.css("right", "-120%");
+      
+})
+
+
