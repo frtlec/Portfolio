86acebf5a0ebdd3097504906d7b5ea674b3dadac
zafer kırık <zafer.krk@hotmail.com>
2022-04-30 12:41:01 +0300

    Remove UseHttpsRedirection

--- MailSender/Portfolio.Services.MailSender/Startup.cs
+++ MailSender/Portfolio.Services.MailSender/Startup.cs
@@ -20,100 +20,102 @@ using System.Threading.Tasks;
 
 namespace Portfolio.Services.MailSender
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
-                   AddJwtBearer(opt =>
-                   {
-                       opt.Authority = Configuration["IdentityServerURL"];
-                       opt.Audience = "resource_mailsender";
-                       opt.RequireHttpsMetadata = false;
-                   });
-            services.AddAuthorization(opts =>
-            {
-                opts.AddPolicy("ReadAndWrite", policy =>
-                {
-                    policy.RequireClaim("scope", new[] { "selin.ozoglu.com.work.write", "selin.ozoglu.com.work.read" });
-                });
-                opts.AddPolicy("WriteEditWork", policy =>
-                {
-                    policy.RequireClaim("scope", new[] { "selin.ozoglu.com.work.write" });
-                });
-            });
-            services.AddMassTransit(x =>
-            {
-                x.AddConsumer<ContactMailSenderCommandConsumer>();
-                // Default Port : 5672
-                x.UsingRabbitMq((context, cfg) =>
-                {
-                    cfg.Host(Configuration["RabbitMQUrl"], "/", host =>
-                    {
-                        host.Username("guest");
-                        host.Password("guest");
-                    });
-                    cfg.ReceiveEndpoint("contact-mail-sender-command-consumer", e => {
-                        e.ConfigureConsumer<ContactMailSenderCommandConsumer>(context);
-                    });
-                });
-            });
-            services.AddCors(options =>
-            {
-                options.AddPolicy(
-                    name: "AllowOrigin",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                    });
-            });
-            services.AddTransient<IMailSettingService, MailSettingService>();
-            services.AddTransient<IContactService, ContactService>();
-            services.AddTransient<IMailSenderService, MailSenderService>();
-            services.AddAutoMapper(typeof(Startup));
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Portfolio.Services.MailSender", Version = "v1" });
-            });
-            services.Configure<DataBaseSettings>(Configuration.GetSection("DatabaseSettings"));
-            services.AddSingleton<IDataBaseSettings>(sp =>
-            {
-                return sp.GetRequiredService<IOptions<DataBaseSettings>>().Value;
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portfolio.Services.MailSender v1"));
-            }
-
-            app.UseCors("AllowOrigin");
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+             AddJwtBearer(opt =>
+             {
+               opt.Authority = Configuration["IdentityServerURL"];
+               opt.Audience = "resource_mailsender";
+               opt.RequireHttpsMetadata = false;
+             });
+      services.AddAuthorization(opts =>
+      {
+        opts.AddPolicy("ReadAndWrite", policy =>
+              {
+            policy.RequireClaim("scope", new[] { "selin.ozoglu.com.work.write", "selin.ozoglu.com.work.read" });
+          });
+        opts.AddPolicy("WriteEditWork", policy =>
+              {
+            policy.RequireClaim("scope", new[] { "selin.ozoglu.com.work.write" });
+          });
+      });
+      services.AddMassTransit(x =>
+      {
+        x.AddConsumer<ContactMailSenderCommandConsumer>();
+              // Default Port : 5672
+              x.UsingRabbitMq((context, cfg) =>
+              {
+            cfg.Host(Configuration["RabbitMQUrl"], "/", host =>
+                  {
+                host.Username("guest");
+                host.Password("guest");
+              });
+            cfg.ReceiveEndpoint("contact-mail-sender-command-consumer", e =>
+            {
+              e.ConfigureConsumer<ContactMailSenderCommandConsumer>(context);
+            });
+          });
+      });
+      services.AddCors(options =>
+      {
+        options.AddPolicy(
+                  name: "AllowOrigin",
+                  builder =>
+                  {
+                builder.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+              });
+      });
+      services.AddTransient<IMailSettingService, MailSettingService>();
+      services.AddTransient<IContactService, ContactService>();
+      services.AddTransient<IMailSenderService, MailSenderService>();
+      services.AddAutoMapper(typeof(Startup));
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Portfolio.Services.MailSender", Version = "v1" });
+      });
+      services.Configure<DataBaseSettings>(Configuration.GetSection("DatabaseSettings"));
+      services.AddSingleton<IDataBaseSettings>(sp =>
+      {
+        return sp.GetRequiredService<IOptions<DataBaseSettings>>().Value;
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+
+      app.UseDeveloperExceptionPage();
+      if (env.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portfolio.Services.MailSender v1"));
+      }
+
+      app.UseCors("AllowOrigin");
+      //app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
--- Services/PhotoStock/Portfolio.Services.PhotoStock/Startup.cs
+++ Services/PhotoStock/Portfolio.Services.PhotoStock/Startup.cs
@@ -15,76 +15,77 @@ using System.Threading.Tasks;
 
 namespace Portfolio.Services.PhotoStock
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
-                  AddJwtBearer(opt =>
-                  {
-                      opt.Authority = Configuration["IdentityServerURL"];
-                      opt.Audience = "resource_photostock";
-                      opt.RequireHttpsMetadata = false;
-                  });
-
-            services.AddAuthorization(opts =>
-            {
-                opts.AddPolicy("ReadAndWrite", policy =>
-                {
-                    policy.RequireClaim("scope", new[] { "selin.ozoglu.com.work.write", "selin.ozoglu.com.work.read" });
-                });
-                opts.AddPolicy("WriteEditWork", policy =>
-                {
-                    policy.RequireClaim("scope", new[] { "selin.ozoglu.com.work.write" });
-                });
-            });
-            services.AddCors(options =>
-            {
-                options.AddPolicy(
-                    name: "AllowOrigin",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                    });
-            });
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Portfolio.Services.PhotoStock", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portfolio.Services.PhotoStock v1"));
-            }
-
-            app.UseCors("AllowOrigin");
-            app.UseStaticFiles();
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+            AddJwtBearer(opt =>
+            {
+              opt.Authority = Configuration["IdentityServerURL"];
+              opt.Audience = "resource_photostock";
+              opt.RequireHttpsMetadata = false;
+            });
+
+      services.AddAuthorization(opts =>
+      {
+        opts.AddPolicy("ReadAndWrite", policy =>
+              {
+            policy.RequireClaim("scope", new[] { "selin.ozoglu.com.work.write", "selin.ozoglu.com.work.read" });
+          });
+        opts.AddPolicy("WriteEditWork", policy =>
+              {
+            policy.RequireClaim("scope", new[] { "selin.ozoglu.com.work.write" });
+          });
+      });
+      services.AddCors(options =>
+      {
+        options.AddPolicy(
+                  name: "AllowOrigin",
+                  builder =>
+                  {
+                builder.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+              });
+      });
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Portfolio.Services.PhotoStock", Version = "v1" });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseDeveloperExceptionPage();
+      if (env.IsDevelopment())
+      {
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portfolio.Services.PhotoStock v1"));
+      }
+
+      app.UseCors("AllowOrigin");
+      app.UseStaticFiles();
+      //app.UseHttpsRedirection();
+
+      app.UseRouting();
+      app.UseAuthentication();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
