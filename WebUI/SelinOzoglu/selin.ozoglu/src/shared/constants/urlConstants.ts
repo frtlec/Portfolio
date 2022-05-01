f4d1f6d2eae5fc6c11e40dc8ca01373f017e03fb
zafer kırık <zafer.krk@hotmail.com>
2022-05-01 19:14:43 +0300

    url fix

--- WebUI/SelinOzoglu/selin.ozoglu/src/shared/constants/urlConstants.ts
+++ WebUI/SelinOzoglu/selin.ozoglu/src/shared/constants/urlConstants.ts
@@ -17,17 +17,17 @@
 // const IDENTITY4_SERVER_BASE_URL="http://localhost:5001";
 
 
-const PHOTO_STOCK_API_BASE_URL="http://localhost:5100/services/PhotoStock";
-const WORK_API_BASE_URL="http://localhost:5100/services/workitems";
-const PHOTO_STOCK_API_PHOTOS_FILE_URL="http://localhost:5100/services/PhotoStock/photos";
-const MAIL_SENDER_API_BASE_URL="http://localhost:5100/services/MailSender";
-const IDENTITY4_SERVER_BASE_URL="http://localhost:5101";
+// const PHOTO_STOCK_API_BASE_URL="http://localhost:5100/services/PhotoStock";
+// const WORK_API_BASE_URL="http://localhost:5100/services/workitems";
+// const PHOTO_STOCK_API_PHOTOS_FILE_URL="http://localhost:5100/services/PhotoStock/photos";
+// const MAIL_SENDER_API_BASE_URL="http://localhost:5100/services/MailSender";
+// const IDENTITY4_SERVER_BASE_URL="http://localhost:5101";
 
-// const PHOTO_STOCK_API_BASE_URL="http://107.173.251.36:5100/services/PhotoStock";
-// const WORK_API_BASE_URL="http://107.173.251.36:5100/services/workitems";
-// const PHOTO_STOCK_API_PHOTOS_FILE_URL="http://107.173.251.36:5104/photos/";
-// const MAIL_SENDER_API_BASE_URL="http://107.173.251.36:5100/services/MailSender";
-// const IDENTITY4_SERVER_BASE_URL="http://107.173.251.36:5101";
+const PHOTO_STOCK_API_BASE_URL="http://107.173.251.36:5100/services/PhotoStock";
+const WORK_API_BASE_URL="http://107.173.251.36:5100/services/workitems";
+const PHOTO_STOCK_API_PHOTOS_FILE_URL="http://107.173.251.36:5104/photos/";
+const MAIL_SENDER_API_BASE_URL="http://107.173.251.36:5100/services/MailSender";
+const IDENTITY4_SERVER_BASE_URL="http://107.173.251.36:5101";
 export {
     PHOTO_STOCK_API_BASE_URL,
     WORK_API_BASE_URL,
